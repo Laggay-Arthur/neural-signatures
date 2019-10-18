4ae9f03acbfb027502948c6128c2905567a1f6ba
Laggay-Arthur <laggay.arthur@yandex.ru>
2019-10-19 02:02:36 +0300

    добавил библиотеку tesseract и реализовал считывание текста с изображения

--- neural-signatures/MainWindow.xaml.cs
+++ neural-signatures/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tesseract;
+using Microsoft.Win32;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace neural_signatures
 {
@@ -20,9 +25,49 @@ namespace neural_signatures
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+       private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            progressbar1.Value = 0;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+           if( openFileDialog.ShowDialog() == true)
+            {
+                
+                while (progressbar1.Value < 50)
+                {
+                    progressbar1.Value++;
+                    
+                }
+
+
+                /*Thread thread = new Thread(new ParameterizedThreadStart(UpdateTextWrong));
+                thread.Start(openFileDialog);*/
+                new Thread(() =>
+                {
+                    this.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+               (ThreadStart)delegate ()
+               {
+                   //OpenFileDialog openFileDialog = (OpenFileDialog)obj;
+                   Bitmap img = new Bitmap(openFileDialog.FileName);
+                   TesseractEngine ocr = new TesseractEngine("./tessdata", "eng+rus", EngineMode.Default);
+                   var page = ocr.Process(img);
+                   progressbar1.Value = 100;
+                   textbox1.Text = page.GetText();
+               });
+                }).Start();
+
+             
+            }
+        }
+       
+     
+
     }
 }
