d16ce95fa6b751863460af7d5869bdf5a90adbe5
Medoo48 <artem_1889@mail.ru>
2019-10-26 10:59:03 +0300

    Добавил новые окна

---
+++ neural-signatures/SetPositionByHand.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+
+namespace neural_signatures
+{
+    public partial class SetPositionByHand : Window
+    {
+        public SetPositionByHand(string fileName)
+        {
+            InitializeComponent();
+            loaded_img.Source = new BitmapImage(new Uri(fileName));
+        }
+
+        void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+    }
+}
---
+++ neural-signatures/TrainWebWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+
+namespace neural_signatures
+{
+    public partial class TrainWebWindow : Window
+    {
+        public TrainWebWindow() { InitializeComponent(); }
+
+
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        private void LoadTrain_Click(object sender, RoutedEventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                loaded_img.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+
+        void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+    }
+}
